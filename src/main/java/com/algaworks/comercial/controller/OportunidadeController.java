7eaa91e8976f9608dbc115026c2145e5f2ab4fc4
FernandaRSilva <fernandareeis@yahoo.com>
2020-01-23 13:14:39 -0300

    Utilização de anotações do JPA na classe Oportunidade para Mapeamento Objeto-Relacional, criação do Repositório e implementação da funcionalidade de listagem de oportunidades.

--- src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
+++ src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
@@ -1,26 +1,24 @@
 package com.algaworks.comercial.controller;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.comercial.model.Oportunidade;
+import com.algaworks.comercial.repository.OportunidadeRepository;
 
 @RestController
 @RequestMapping("/oportunidades")
 public class OportunidadeController {
 	
+	@Autowired
+	private OportunidadeRepository oportunidades;
+	
 	@GetMapping
 	public List<Oportunidade> listar() {
-		Oportunidade oportunidade = new Oportunidade();
-		oportunidade.setId(1L);
-		oportunidade.setDescricao("Desenvolvimento de ERP com Angular e Spring");
-		oportunidade.setNomeProspecto("Grupo Logísstica Brasil");
-		oportunidade.setValor(new BigDecimal(490000));
-		return Arrays.asList(oportunidade);
+		return oportunidades.findAll();
 	}
 }
